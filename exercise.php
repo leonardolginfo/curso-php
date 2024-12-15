5eadbbac76a56bcb338e61be85dab68bbdfe7140
leonardolginfo <leonardo@lginfo.com.br>
2024-12-15 17:24:41 -0300

    estrutura base page exercise e css menu navegacao sem php ainda

--- exercise.php
+++ exercise.php
@@ -8,15 +8,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/exercise.css">
     <title>Curso PHP</title>
 </head>
 
-<body>
+<body class="exercise">
 
     <header class="cabecalho">
         <h1>Exercício</h1>
         <h2>Visualização do exercício</h2>
     </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem Formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
     <main class="principal">
         <div class="conteudo">
         </div>
