6916d4baf66cd579a147ac02fd83648a86398ebb
leonardolginfo <leonardo@lginfo.com.br>
2024-12-16 19:36:34 -0300

    abrindo sem formatacao em nova guia

--- exercise.php
+++ exercise.php
@@ -19,7 +19,7 @@
         <h2>Visualização do exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href=<?= "{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem Formatação</a>
+        <a href=<?= "{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde" target="_blank">Sem Formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
