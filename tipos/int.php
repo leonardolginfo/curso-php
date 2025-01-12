7a061f087473f11a9115dfbbc90ea087dc33766d
leonardolginfo <leonardo@lginfo.com.br>
2025-01-12 08:52:42 -0300

    Pequeno exemplo de tipo INT

--- tipos/int.php
+++ tipos/int.php
@@ -1,1 +1,12 @@
 <div class="titulo">Tipo INT em PHP</div>
+<?php  echo 'Literal '. 11;
+echo '<br>';
+var_dump(11);
+echo '<br>';
+echo 'Tamanho máximo de um INT nesta máquina: '.PHP_INT_MAX , '<br>';
+echo 'Tamanho mínimo de um INT nesta máquina: ' . PHP_INT_MIN, '<br>';
+echo 'Impressão de 17: ' . 17 , ' Decimal <br>';
+echo ' Impressão de 017 - base octal - sempre será impresso a base decimal que é: ', 017 , ' <br>';
+echo 'Impressão na base binária de 0b11: ' . 0b11 , '<br>'; //base binária
+echo 'Impressão hexadecimal 0x117acf0 que é:' . 0x117acf0;
+?>
